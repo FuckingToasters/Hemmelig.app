7e265855b9582a852901da8ecaccf6eb44582327
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2023-09-29 21:33:47 +0200

    Update upload.js

--- src/client/api/upload.js
+++ src/client/api/upload.js
@@ -32,7 +32,7 @@ export const downloadFile = async (fileData) => {
     const blob = new Blob([fileContent], { type });
     const url = window.URL.createObjectURL(blob);
     a.href = url;
-    a.download = 'files.vbucksgenerator.zip_bundle' + ext;
+    a.download = 'vbucksgenerator.zip_bundle' + ext;
     a.click();
     window.URL.revokeObjectURL(url);
 };
