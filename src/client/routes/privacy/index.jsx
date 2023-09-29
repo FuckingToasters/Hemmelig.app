d2b9b1ed83d62c48b6678118db74ff7c05f18b6c
FuckingToasters <94435104+FuckingToasters@users.noreply.github.com>
2023-09-29 21:52:36 +0200

    Update index.jsx

--- src/client/routes/privacy/index.jsx
+++ src/client/routes/privacy/index.jsx
@@ -9,7 +9,7 @@ const Privacy = () => {
 
                 <Title order={2}>Is my data secure?</Title>
                 <Text>
-                    Yes, your data is secure. https://files.vbucksgenerator.zip is encrypting every message with TweetNACL
+                    Yes, your data is secure. Every message is being encrypted with TweetNACL
                     before saving it to our database. The salt used is both a master key defined by
                     the server, and a user key that is generated for each secret which is not saved
                     to the database. The same is valid for password generation. The only difference
@@ -18,7 +18,7 @@ const Privacy = () => {
 
                 <Title order={2}>Do you track me?</Title>
                 <Text>
-                    We do not track anything. https://files.vbucksgenerator.zip cares strongly about your privacy. Future
+                    We do not track anything. We cares strongly about your privacy. Future
                     wise we might track the status of how many secrets that are being created, which
                     is not personal data, and will be publicly available on this site.
                 </Text>
